e9d7db67e04b38421e3dcaa49fc1bcf5224a587a
Mahmoud Fawzy <graffitotrenders@gmail.com>
2021-09-09 03:10:39 +0200

    first commit

---
+++ greet-the-user/# greet the user.py
@@ -0,0 +1,10 @@
+# Greet the user 
+print("Hi!")
+name = input("What's ur name")
+print("Nice to meet u", name)
+
+answer = input("R u enjoying the course?")
+if answer == "Yes":
+    print(" Great! ")
+else: 
+      print("Sad for that!")
