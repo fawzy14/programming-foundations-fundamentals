07e655c64feb7621e7808a0cbe12a567f4d7ec9c
Mahmoud Fawzy <graffitotrenders@gmail.com>
2021-09-11 19:35:32 +0200

    upload gussing game

---
+++ gussing-game/gussing-game.py
@@ -0,0 +1,9 @@
+print("Hi!")
+name = input(" What is ur favorite food? ")
+
+if name == "koshary":
+    print("Yep, so amazing!")
+else: 
+    print("Yuck! that's not it!")
+
+print("Thanks for playing!")
