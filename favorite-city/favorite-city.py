6f8dec71128cb9fa904de379818defc2050ec552
Mahmoud Fawzy <graffitotrenders@gmail.com>
2021-09-13 02:14:48 +0200

    favorite city

---
+++ favorite-city/favorite-city.py
@@ -0,0 +1,7 @@
+# print out favorite city using def function
+def favorite_city(name):
+    print("My favorite city is", name)
+
+favorite_city("Cairo, Egypt")
+favorite_city("Aswan, Egypt")
+favorite_city("Alexandria, Egypt")
